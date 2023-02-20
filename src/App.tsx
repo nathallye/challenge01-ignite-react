09f2444d568399ddacb53df2d6c75720894baa6d
Nathallye Bacelar <86172286+nathallye@users.noreply.github.com>
2023-02-19 22:37:40 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -8,18 +8,11 @@ import styles from "./App.module.css";
 import "./global.css";
 
 const App = () => {
-  //const [count, setCount] = useState(0)
-
   return (
 
     <div>
       <Header />
       <TaskList />
-      {/*
-      <button onClick={() => setCount((count) => count + 1)}>
-        count is {count}
-      </button>
-      */}
     </div>
   )
 }
